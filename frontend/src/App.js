d815e44c1da912904905c2a3f9ba46d7cc1f99db
sa60 <sa60@illinois.edu>
2023-10-21 20:36:04 -0500

    Backend Complete

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <AppStyled className="App">
+
+    </AppStyled>
+
+  );
+}
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: 
+
+`;
+
+export default App;
